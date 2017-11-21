378ed37cfa4e682c8fae33d1ee025f4fbc3501e5
niall devery <G00338932@gmit.ie>
2017-11-21 17:26:51 +0000

    alterations to the html

--- eliza.go
+++ eliza.go
@@ -3,32 +3,17 @@ package main
 import (
 	//"html/template"//add html/template package 
 	"net/http"
+	"log"
 
 )
-	
-type myMsg struct {
-	Input string
-	Output string
-	Previous string
-}
-
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	//serve the homepage.html file
-	http.ServeFile(w, r, "eliza.html")
-}
-
-func chatHandler(w http.ResponseWriter, r *http.Request) {
-	response := "hello"
-	message := r.FormValue("chat")
-	previous := input 
-
-}//chatHandler
 
 func main() {
 	// handles root page
-	http.HandleFunc("/", requestHandler)
-
-	//handle /chat page
-	http.HandleFunc("/chat", chatHandler)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":8080", nil)
+	
+	log.Println("Listening...")
+	http.ListenAndServe(":8080", nil)
+	
+	
 }
