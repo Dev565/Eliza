d0623c3b3d8a290e70e745efc2be795c03e4f370
niall devery <G00338932@gmit.ie>
2017-11-21 18:47:33 +0000

    Creating reply functions

--- eliza.go
+++ eliza.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	//"html/template"//add html/template package 
-	"net/http"
-	"log"
+	//"net/http"
+	//"log"
 	"fmt"
 	"math/rand"
 	"time"
 	"regexp"
 
 )
+func handler(w htt.ResponseWriter, r *http.Request){
+		userInput := r.URL.Query().Get("user-input")
+		response := elizaResponse(userInput)
+}
+
 func elizaResponse(input string) string{
+	response := createReply()
+
 	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched{
 		return "why dont you tell me about your father?"
 	}	
@@ -29,13 +36,64 @@ func elizaResponse(input string) string{
 	return answers[rand.Intn(len(answers))]
 }	
 
+func createReply(){
+	
+}
+
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano()) // Try changing this number!
+
+	fmt.Println(" Input : " + "People say I look like both my mother and father.")
+		fmt.Println(" Output : " + elizaResponse("People say I look like both my mother and father"))
+		fmt.Println()
+
+
+		
+		fmt.Println(" Input : " + "Father was a teacher.")
+		fmt.Println(" Output : " + elizaResponse("Father was a teacher"))
+
+		
+		fmt.Println(" Input : " + "I was my father’s favourite.")
+		fmt.Println(" Output : " + elizaResponse("I was my father’s favourite."))
+		fmt.Println()
+
+
+		fmt.Println(" Input : " + "Im looking forward to the weekend.")
+		fmt.Println(" Output : " + elizaResponse("I’m looking forward to the weekend."))
+		fmt.Println()
+
+
+		fmt.Println(" Input : " + "My grandfather was French!")
+		fmt.Println(" Output : " + elizaResponse("My grandfather was French!"))
+		fmt.Println()
+
+		
+		fmt.Println(" Input :" + "I am happy")
+		fmt.Println(" Output :" + elizaResponse("I am happy"))
+		fmt.Println()
+
+
+		fmt.Println(" Input :" + "I am not happy with your responses. ")
+		fmt.Println(" Output :" + elizaResponse("I am not happy with your responses. "))
+		fmt.Println()
+
+				
+		fmt.Println(" Input :" + " I am not sure that you understand the effect that your questions are having on me.")
+		fmt.Println(" Output :" + elizaResponse("I am not sure that you understand the effect that your questions are having on me."))
+		fmt.Println()
+
+		
+		fmt.Println(" Input :" + "I am supposed to just take what you’re saying at face value?")
+		fmt.Println(" Output :" + elizaResponse("I am supposed to just take what you’re saying at face value? "))
+		fmt.Println()
+
+	
 	// handles root page
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	/*http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":8080", nil)
 	
 	log.Println("Listening...")
 	http.ListenAndServe(":8080", nil)
-	
+	*/
 	
 }
